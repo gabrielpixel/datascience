792f382d8f7a41e4649bd875cf196389d987996a
gabriel <gabriel@mail.com>
2020-06-23 13:53:49 -0300

    second commit with directorie NaN

---
+++ NaN/naornan.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+data_frame = pd.DataFrame(np.random.randn(5,3), index=['a','c','e','f',
+'h'],columns=['one','two','three'])
+data_frame = data_frame.reindex(['a','b','c','d','e','f','g','h',])
+print(data_frame)
---
+++ NaN/naornanfillna.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+data_frame = pd.DataFrame(np.random.randn(5,3), index=['a','c','e','f',
+'h'],columns=['one','two','three'])
+data_frame = data_frame.reindex(['a','b','c','d','e','f','g','h',])
+print(data_frame)
+
+print("NaN replace with '0': ")
+print(data_frame.fillna(0))
